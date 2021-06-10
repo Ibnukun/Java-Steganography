a194e49de21b7c2842dcf6d8fbb58ec6576b4f3a
Ibnukun <52390086+Ibnukun@users.noreply.github.com>
2021-06-10 20:05:47 +0700

    Add files via upload

---
+++ steganografi/src/main/java/watermarking/App.java
@@ -0,0 +1,14 @@
+package watermarking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+    NewJFrame myFrame = new NewJFrame();
+    myFrame.setVisible(true);
+    myFrame.setLocationRelativeTo(null);
+    }
+}
+    
